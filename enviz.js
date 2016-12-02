ee98964a05aa5ea7d136b4bfbb0b4af6132b2c0a
Adrian Barrera <admbarre@ucsc.edu>
2016-12-01 16:47:21 -0800

    Added code for generating links list and code for drawing links.

--- enviz.js
+++ enviz.js
@@ -2,47 +2,67 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
-function draw(data){
+function draw(courses, links){
     let node = svg.append("g")
         .attr("class", "nodes")
         .selectAll("circle")
-        .data(data)
+        .data(courses)
         .enter().append("circle")
-        .attr("r", d => Math.sqrt(d.capacity)/.2)
+        .attr("r", d => Math.sqrt(d.capacity))
         .attr("fill", "red")
         .attr("stroke", "black")
         .attr("stroke-width", "1.5px");
     
-//    let link = svg.append("g")
-//        .attr("class", "links")
-//        .selectAll("line")
-//        .data(data)
-//        .enter().append("line")
-//        .attr("stroke-width", "1.5px")
-//        .attr("stroke", "cyan");
+    let link = svg.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(links)
+        .enter().append("line")
+        .attr("stroke-width", "1.5px")
+        .attr("stroke", "black");
 
-    let sim = d3.forceSimulation(data)
+    let sim = d3.forceSimulation(courses)
         .force("center", d3.forceCenter(width/2, height/2))
-        .force("collide", d3.forceCollide().radius(d=>Math.sqrt(d.capacity)/.2))
-        .force("charge", d3.forceManyBody().strength(30))
+        .force("collide", d3.forceCollide()
+               .radius(d=>Math.sqrt(d.capacity)))
+        .force("charge", d3.forceManyBody()
+               .strength(30))
+        .force("link", d3.forceLink()
+               .id((d)=>d.id))
         .on("tick", ticked);
     
+    sim.force("link")
+        .links(links);
+    
     function ticked(){
-//        link
-//            .attr("x1", data.x)
-//            .attr("y1", data.y)
-//            .attr("x2", d => d.x)
-//            .attr("y2", d => d.y);
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
         node        
             .attr("cx", d => d.x)
             .attr("cy", d => d.y);
     }
 }
 
-d3.json("csbs.json", (error, data)=>{
+function genLinks(courses){
+    links = [];
+    courses.forEach((course)=>{
+        course.prerequisites.forEach((pre)=>{
+           links.push({"source": course.id, "target": pre});
+        });
+    });
+
+    return links;
+}
+
+d3.json("csbs.json", (error, courses)=>{
     if(error){
         console.log(error);
     }else{
-        draw(data);
+        links = genLinks(courses);
+        console.log(links);
+        draw(courses, links);
     }
 });
